712d38125f835960b0ed07d37ff60917b89b409b
JulietaOk <julieta.lopez@live.com>
2022-09-06 21:55:42 -0300

    Angular

--- Portfolio-AP/src/app/app.module.ts
+++ Portfolio-AP/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    BannerComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
